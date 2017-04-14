01846fd64fdc4f98cddbd0f3f6e1f17d8a9498a5
parloti <parloti@gmail.com>
2017-04-14 23:31:55 +0200

    Update social-tab in modal contacts

--- script.js
+++ script.js
@@ -1,61 +1,190 @@
 (function (angular) {
-    'use strict';
-    angular.module('portfolio', ['ngAnimate','ng.deviceDetector']).directive('onFinishRender', function ($timeout) {
-        return {
-            restrict: 'A',
-            link: function (scope, element, attr) {
-                if (scope.$last === true) {
-                    $timeout(function () {
-                        scope.$emit(attr.onFinishRender);
-                    });
-                }
-            }
-        }
-    }).controller('repeatController', ['$scope','deviceDetector', function ($scope,deviceDetector) {
-        console.log(deviceDetector);
-        $scope.browser=deviceDetector.browser;
-        $scope.boards = [
-            {
-                name: 'About',
-                iconClass: 'fa fa-info-circle icon-white',
-                isVisible: true
-            },
-            {
-                name: 'Skills',
-                iconClass: 'fa fa-wrench icon-white',
-                isVisible: false
-            },
-            {
-                name: 'Projects',
-                iconClass: 'fa fa-cogs icon-white',
-                isVisible: false
-            },
-            {
-                name: 'Testimonials',
-                iconClass: 'fa fa-comments icon-white',
-                isVisible: false
-            },
-            {
-                name: 'Articles',
-                iconClass: 'fa fa-pencil-square-o icon-white',
-                isVisible: false
-            },
-            {
-                name: 'Contacts',
-                iconClass: 'fa fa-address-book-o icon-white',
-                isVisible: false
-            }
-        ];
-        $scope.open = function (board) {
-            console.log(board);
-        }
-        $scope.$on('renderedModal', function (ngRepeatFinishedEvent) {
-            console.log(ngRepeatFinishedEvent);
-            console.log("done");
-            $('#ContactsModal').modal('show');
-	        $('nav a:eq(1)').tab('show')
-        })
-
-
-    }]);
+	'use strict';
+	angular.module('portfolio', ['ngAnimate', 'ng.deviceDetector']).directive('onFinishRender', function ($timeout) {
+		return {
+			restrict: 'A',
+			link: function (scope, element, attr) {
+				if (scope.$last === true) {
+					$timeout(function () {
+						scope.$emit(attr.onFinishRender);
+					});
+				}
+			}
+		}
+	}).controller('repeatController', ['$scope', 'deviceDetector', function ($scope, deviceDetector) {
+		console.log(deviceDetector);
+		$scope.browser = deviceDetector.browser;
+		$scope.boards = [
+			{
+				name: 'About',
+				iconClass: 'fa fa-info-circle icon-white',
+				isVisible: true,
+				isActive: true
+			},
+			{
+				name: 'Skills',
+				iconClass: 'fa fa-wrench icon-white',
+				isVisible: true,
+				isActive: true
+			},
+			{
+				name: 'Projects',
+				iconClass: 'fa fa-cogs icon-white',
+				isVisible: true,
+				isActive: true
+			},
+			{
+				name: 'Testimonials',
+				iconClass: 'fa fa-comments icon-white',
+				isVisible: false,
+				isActive: true
+			},
+			{
+				name: 'Articles',
+				iconClass: 'fa fa-pencil-square-o icon-white',
+				isVisible: false,
+				isActive: true
+			},
+			{
+				name: 'Contacts',
+				iconClass: 'fa fa-address-book-o icon-white',
+				isVisible: true,
+				isActive: true,
+				navigation: [
+					{
+						name: 'QR Code',
+						href: '#qr-code-panel',
+						iconClass: 'fa fa-user-circle-o',
+						isVisible: true,
+						isActive: true
+					},
+					{
+						name: 'vCard',
+						href: '#vcard-panel',
+						iconClass: 'fa fa-address-card-o',
+						isVisible: true,
+						isActive: true
+					},
+					{
+						name: 'Social',
+						href: '#social-panel',
+						iconClass: 'fa fa-share-alt',
+						isVisible: true,
+						isActive: true
+					},
+					{
+						name: 'Message',
+						href: '#message-panel',
+						iconClass: 'fa fa-envelope-o',
+						isVisible: true,
+						isActive: true
+					}
+				],
+				content: [
+					{
+						caption: 'Add Me to Your Contacts List'
+					},
+					{
+						caption: 'Add Me to Your Contacts List'
+					},
+					{
+						caption: 'Add Me to Your Contacts List'
+					},
+					{
+						caption: 'Add Me to Your Contacts List'
+					},
+				]
+			}
+		];
+		$scope.socialCards = [
+			{
+				name: 'LinkedIn',
+				href: "linkedin.com/in/parloti/",
+				iconClass: 'fa fa-linkedin',
+				isVisible: true,
+				isActive: true
+			},
+			{
+				name: 'Xing',
+				href: "xing.com/profile/Alex_Parloti",
+				iconClass: 'fa fa-xing',
+				isVisible: true,
+				isActive: true
+			},
+			{
+				name: 'GitHub Profile',
+				href: "github.com/parloti",
+				iconClass: 'fa fa-github',
+				isVisible: true,
+				isActive: true
+			},
+			{
+				name: 'GitHub Portfolio',
+				href: "parloti.github.io",
+				iconClass: 'fa fa-github-alt',
+				isVisible: true,
+				isActive: true
+			},
+			{
+				name: 'Free Code Camp',
+				href: "freecodecamp.com/parloti",
+				iconClass: 'fa fa-free-code-camp',
+				isVisible: true,
+				isActive: true
+			},
+			{
+				name: 'Meetup',
+				href: "meetup.com/members/212267196/",
+				iconClass: 'fa fa-meetup',
+				isVisible: true,
+				isActive: true
+			},
+			{
+				name: 'Twitter',
+				href: "twitter.com/parloti",
+				iconClass: 'fa fa-twitter',
+				isVisible: true,
+				isActive: true
+			},
+			{
+				name: 'Google+',
+				href: "plus.google.com/+AlexdaSilvaParloti",
+				iconClass: 'fa fa-google-plus-circle',
+				isVisible: false,
+				isActive: true
+			},
+			{
+				name: 'Facebook',
+				href: "facebook.com/parloti",
+				iconClass: 'fa fa-facebook',
+				isVisible: false,
+				isActive: true
+			},
+			{
+				name: 'Instagram',
+				href: "instagram.com/alex.parloti/",
+				iconClass: 'fa fa-instagram',
+				isVisible: false,
+				isActive: true
+			},
+			{
+				name: 'YouTube',
+				href: "youtube.com/user/parloti",
+				iconClass: 'fa fa-youtube-play',
+				isVisible: false,
+				isActive: true
+			}
+		];
+		$scope.open = function (board) {
+			console.log(board);
+		}
+		$scope.$on('renderedModal', function (ngRepeatFinishedEvent) {
+			console.log(ngRepeatFinishedEvent);
+			console.log("done");
+			$('#ContactsModal').modal('show');
+			$('nav a:eq(3)').tab('show')
+		})
+		
+		
+	}]);
 })(window.angular);
--- src2cdn.js
+++ src2cdn.js
@@ -1,18 +1,101 @@
-debugger;
-var fs = require("fs");
+
 debugger;
 !function () {
-	class Files {
-		constructor(args) {
-			this.type = args.type;
-			this.relativePath = args.relativePath;
+	class Directory {
+		constructor(params) {
+			debugger;
+			this.relativePath = params.relativePath;
+			this.subDirectories = [];
+			this.files = [];
+			this.readDirectory();
+		}
+		
+		readDirectory() {
+			debugger;
+			let fs = require('fs');
+			let path = require('path');
+			fs.readdir(this.relativePath, (error, files) => {
+				debugger;
+				if (error) {
+					throw new Error(error);
+				}
+				this.readStat(files);
+			});
+		}
+		
+		readStat(files) {
+			debugger;
+			let fs = require('fs');
+			let path = require('path');
+			files.forEach((name) => {
+				let filePath = path.join(this.relativePath, name);
+				fs.stat(filePath, (error, stats) => {
+					debugger;
+					let args = {
+						name: name,
+						filePath: filePath,
+						stats: stats
+					}
+					this.checkStoringType(args);
+				});
+			})
+		}
+		
+		checkStoringType(params) {
+			debugger;
+			let isFile = params.stats.isFile();
+			let isDirectory = params.stats.isDirectory();
+			let args = {
+				name: params.name,
+				relativePath: params.filePath
+			}
+			if (isFile) {
+				this.pushFile(args);
+			}
+			else if (isDirectory) {
+				this.pushDirectory(args);
+			}
+			else {
+				let error = `${filePath} is not a file and is not a directory.`;
+				throw new Error(error);
+			}
+		}
+		
+		pushFile(params) {
+			debugger;
+			let args = params;
+			let file = new File(args);
+			this.files.push(file);
+		}
+		
+		pushDirectory(params) {
+			debugger;
+			let args = params;
+			let directory = new Directory(args);
+			this.subDirectories.push(directory);
 		}
 	}
-	class cssFiles extends Files{
+	class File {
+		constructor(params) {
+			debugger;
+			this.name = params.name;
+			this.relativePath = params.relativePath;
+		}
+	}
+	class cssFile extends File {
 	
 	}
 	
-	class htmlFiles extends Files{
+	class htmlFile extends File {
 	
 	}
+	function main() {
+		let args = {
+			relativePath: './'
+		};
+		let Tree = new Directory(args);
+		debugger;
+	}
+	
+	main();
 }();
